d348e4e6884ff6502b8483432971b50115b430cd
Patlolla Pravven <patlollapraveen000@gmail.com>
2025-12-22 10:13:52 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
-# Flask app initialization
+from flask import Flask
+from .config import Config
+from .extensions import db, jwt
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    return app
