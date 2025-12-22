71c4dd1da80ee8bf418bb70d63a8b3be87730527
Patlolla Pravven <patlollapraveen000@gmail.com>
2025-12-22 13:03:45 +0530

    Create expense.py

---
+++ app/models/expense.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+from datetime import datetime
+
+class Expense(db.Model):
+    __tablename__ = "expenses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, nullable=True)
+    amount = db.Column(db.Numeric(10, 2), nullable=False)
+    description = db.Column(db.String(255))
+    expense_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
